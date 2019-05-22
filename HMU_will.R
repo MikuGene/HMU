b2ef616ff0766a7dae594a323e35b68a643a51dc
Lianhao Song <40196567+Citruswalker@users.noreply.github.com>
2019-05-22 18:15:04 +0800

    Update HMU_will.R

--- HMU_will.R
+++ HMU_will.R
@@ -541,7 +541,7 @@ scRNA_3 <- function(x,y = NULL,if_two = F,if_plot = T,name1 = "temp1_sc",name2 =
     HNSC <- FindVariableFeatures(HNSC, selection.method = "vst", nfeatures = 1000*nVar)
     if(if_plot){print(LabelPoints(VariableFeaturePlot(HNSC), points = head(VariableFeatures(HNSC), 10), repel = T))}}
     else{
-      HNSC1 <- CreateSeuratObject(x, name, min.cells = 3, min.features = 200)
+      HNSC1 <- CreateSeuratObject(x, name1, min.cells = 3, min.features = 200)
       HNSC1$Group <- name1
       HNSC1[["percent.mt"]] <- PercentageFeatureSet(object = HNSC1, pattern = Mito[1])
       if(if_plot){print(VlnPlot(HNSC1, c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3,pt.size = 0.2))}
@@ -564,7 +564,7 @@ scRNA_3 <- function(x,y = NULL,if_two = F,if_plot = T,name1 = "temp1_sc",name2 =
       if(!is.null(tem)){cat("well done.\n",file = stderr())}
       rm(tem)
       gc()
-      SCC090 <- CreateSeuratObject(y, name, min.cells = 3, min.features = 200)
+      SCC090 <- CreateSeuratObject(y, name2, min.cells = 3, min.features = 200)
       SCC090$Group <- name1
       SCC090[["percent.mt"]] <- PercentageFeatureSet(object = SCC090, pattern = Mito[2])
       if(if_plot){print(VlnPlot(SCC090, c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3,pt.size = 0.2))}
@@ -668,4 +668,4 @@ Lima <- function(x,y,filt = F,log2FC = 2,padj = 0.01,pval = 0.01){
   return(output)}
 cat(" ","Lima --- done.","\n",file = stderr())
 ## 8a03a29901b31176e32928321b1349e6
-cat(" ","Ready up. Latest update: 2019-05-21-13:54. If any questions, please wechat 18746004617. Email: songlianhao233@gmail.com","\n",file = stderr())
+cat(" ","Ready up. Latest update: 2019-05-21-18:14. If any questions, please wechat 18746004617. Email: songlianhao233@gmail.com","\n",file = stderr())
