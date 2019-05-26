460bbcd427173fb184f275a2b0c86e1afd82d6c8
Lianhao Song <40196567+Citruswalker@users.noreply.github.com>
2019-05-26 16:14:57 +0800

    Update HMU_will.R

--- HMU_will.R
+++ HMU_will.R
@@ -1,12 +1,10 @@
 ## 8a03a29901b31176e32928321b1349e6
-cat("Gift for HMU wei group, 2019-04-09. --- Lianhao Song. If any questions, please wechat 18746004617.","\n")
+cat("Gift for HMU Wei group, 2019-04-09. --- Lianhao Song. If any questions, please wechat 18746004617.","\n")
 library(plyr)
 library(dplyr)
 library(Matrix)
 ## 8a03a29901b31176e32928321b1349e6
-See <- function(x, num1 = 7, num2 = num1){return(x[1:num1, 1:num2])}
-## 8a03a29901b31176e32928321b1349e6
-scRNA_anlysis <- function(path1 = getwd(),path2 = getwd(),mito_name = "^MT\\.",pm = 20,Data_name = "temp",Reso = 0.6,detail = T,nGene_R = c(200,Inf),mito_R = c(-Inf,0.4),PC_M = 7,seed = 233){
+scRNA_2 <- function(path1 = getwd(),path2 = getwd(),mito_name = "^MT\\.",pm = 20,Data_name = "temp",Reso = 0.6,detail = T,nGene_R = c(200,Inf),mito_R = c(-Inf,0.4),PC_M = 7,seed = 233){
   library(Seurat)
   cat(" ","Hello!","Now we focus on:",path1,"\n",file = stderr())
   if(detail){
