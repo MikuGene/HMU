d4ff6acf7fef9a39481ac9881dab9d02bd65b115
Lianhao Song <40196567+Citruswalker@users.noreply.github.com>
2019-10-15 20:27:49 +0800

    Update HMU_will.R

--- HMU_will.R
+++ HMU_will.R
@@ -242,13 +242,13 @@ WGCNA_TOMmap <- function(x,nCPU = 5,Cutsample = T,nGene = 10,mGene = 12,minMD = 
     print("Welcome to Hatsune world. Now let`s cut the samples")
     okline<-scan()
     abline(h = okline, col = "red")
-    print("Can it appropriate?")
+    print("Can it appropriate? If ok, please input yes")
     ok<-scan(what = "character")
     while(!ok=="yes")
     {print("Please try again")
       okline<-scan()
       abline(h = okline, col = "red")
-      print("Can it appropriate?")
+      print("Can it appropriate? If ok, please input yes")
       ok<-scan(what = "character")}
     clust<-cutreeStatic(sampleTree, cutHeight = okline, minSize = 10)
     keepSamples<-(clust==1)
@@ -265,7 +265,7 @@ WGCNA_TOMmap <- function(x,nCPU = 5,Cutsample = T,nGene = 10,mGene = 12,minMD = 
   plot(sft$fitIndices[,1], sft$fitIndices[,5],xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",main = paste("Mean connectivity"))
   text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=0.9,col="red")
   okpower<-sft$powerEstimate
-  print(paste("The power result is",okpower,". Need to modify ?"))
+  print(paste("The power result is",okpower,". Need to modify ? If need, please input yes"))
   judg<-scan(what = "character")
   if(judg=="yes"){
     print("Please input your okpower")
@@ -784,4 +784,4 @@ DEplot <- function(x, pvalue = 0.01, log2FC = 2, plimit = 30, log2limit = 5, col
   if(color == 2){colornum <- c("black", "red")}
   print(ggplot(data=x,aes(x=log2FoldChange, y=-log10(padj),colour=Legend))+ggtitle(Title)+xlab("log2 Foldchange")+ylab("-log10 Padj")+geom_vline(xintercept=c(-log2FC,log2FC),lty=6,col="grey",lwd=0.5)+geom_hline(yintercept = -log10(pvalue),lty=4,col="grey",lwd=0.5)+scale_color_manual(values = colornum)+theme(legend.position="right")+theme_bw()+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),legend.title = element_blank())+xlim(-log2limit,log2limit) + ylim(0,plimit)+theme(plot.title = element_text(hjust = 0.5))+geom_point(alpha=0.4, size=1.2))}
 ## 8a03a29901b31176e32928321b1349e6
-cat(" ","Ready up. Latest update: 2019-10-15-20:06 --- Lianhao Song.","\n","","---If any questions, please wechat 18746004617. Email: songlianhao233@gmail.com","\n",file = stderr())
+cat(" ","Ready up. Latest update: 2019-10-15-20:27 --- Lianhao Song.","\n","","---If any questions, please wechat 18746004617. Email: songlianhao233@gmail.com","\n",file = stderr())
